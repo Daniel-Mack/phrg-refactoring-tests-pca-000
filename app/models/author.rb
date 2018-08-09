bf9647d77d530dc74f9e304effca2c6adfe04e17
Daniel Mack <daniel.mack@powerhrg.com>
2018-08-09 10:59:25 -0400

    finished lab

--- app/models/author.rb
+++ app/models/author.rb
@@ -4,3 +4,10 @@ class Author < ActiveRecord::Base
   validates :name, presence: true, uniqueness: true
   validates :phone_number, length: { is: 10 }
 end
+
+FactoryBot.define do
+  factory :author do
+    name "Carmen"
+    phone_number "1234567890"
+  end
+end
--- app/models/post.rb
+++ app/models/post.rb
@@ -19,3 +19,17 @@ class Post < ActiveRecord::Base
     errors.add(:title, "must be clickbait")
   end
 end
+
+FactoryBot.define do
+  factory :post do
+    title "Secret"
+    content "Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+            Sed dapibus, nulla vel condimentum ornare, arcu lorem hendrerit purus,
+            ac sagittis ipsum nisl nec erat. Morbi porta sollicitudin leo,
+            eu cursus libero posuere ac. Sed ac ultricies ante. Donec nec nulla ipsum.
+            Nunc eleifend, ligula ut volutpat."
+    summary "Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+            Sed dapibus, nulla vel condimentum ornare."
+    category "Fiction"
+  end
+end
